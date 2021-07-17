5a521d81149737dc8276943750c9d12b6f14e7aa
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-17 20:27:32 +0100

    Add automated tests for profile status

--- profiles/tests.py
+++ profiles/tests.py
@@ -2,12 +2,12 @@ import json
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from profiles.api.serializers import ProfileSerializer
-from profiles.models import Profile
+from profiles.api.serializers import ProfileSerializer, ProfileStatusSerializer
+from profiles.models import Profile, ProfileStatus
 
 
 class RegistrationTestCase(APITestCase):
@@ -66,3 +66,42 @@ class ProfileViewSetTestCase(APITestCase):
         response = self.client.put(reverse('profile-detail', kwargs={'pk': 1}),
                                    {'bio': 'hacked!!!'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class ProfileStatusViewSetTestCase(APITestCase):
+
+    url = reverse('status-list')
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='davinci', password='some-very-strong-password')
+        self.status = ProfileStatus.objects.create(
+            user_profile=self.user.profile, status_content='Status test')
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_status_list_authenticated(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_status_list_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_status_create(self):
+        data = {'status_content': 'a new status'}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['user_profile'], 'davinci')
+        self.assertEqual(response.data['status_content'], 'a new status')
+
+    def test_single_status_retrieve(self):
+        serializer_data = ProfileStatusSerializer(instance=self.status).data
+        response = self.client.get(reverse('status-detail', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = json.loads(response.content)
+        self.assertEqual(serializer_data, response_data)
