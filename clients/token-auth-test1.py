761fa9d649ee4d3302ac62ffdf221b9c04426c5c
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-17 12:46:06 +0100

    Install rest_auth, add urls for both authentication methods and test toke authentication via requests

---
+++ clients/token-auth-test1.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def client():
+    credentials = {"username": "pinco", "password": "#######"}
+
+    response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/",
+                             data=credentials)
+
+    print("Status Code: ", response.status_code)
+    response_data = response.json()
+    print(response_data)
+
+
+if __name__ == "__main__":
+    client()
--- profilesapi/urls.py
+++ profilesapi/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/rest-auth/', include('rest_auth.urls')),
 ]
 
 
