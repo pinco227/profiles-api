628762d36a4ff577b0beaa793cec00747409e7ea
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-17 12:56:14 +0100

    Create Profile list view and manual test api token authentication via requests

--- clients/token-auth-test1.py
+++ clients/token-auth-test1.py
@@ -2,10 +2,16 @@ import requests
 
 
 def client():
-    credentials = {"username": "pinco", "password": "#######"}
+    # credentials = {"username": "pinco", "password": "#######"}
 
-    response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/",
-                             data=credentials)
+    # response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/",
+    #                          data=credentials)
+
+    token_h = 'Token 48f7a69ffe8827f52982fd9b3b5c114d98e8ce55'
+    headers = {'Authorization': token_h}
+
+    response = requests.get('http://127.0.0.1:8000/api/profiles/',
+                            headers=headers)
 
     print("Status Code: ", response.status_code)
     response_data = response.json()
