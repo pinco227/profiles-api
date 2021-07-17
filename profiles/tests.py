e04893de7304ad9cec4ebaef3bedc5b4d30316a1
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-17 20:34:20 +0100

    refactor imports

--- profiles/tests.py
+++ profiles/tests.py
@@ -2,12 +2,12 @@ import json
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from rest_framework import serializers, status
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from profiles.api.serializers import ProfileSerializer, ProfileStatusSerializer
-from profiles.models import Profile, ProfileStatus
+from profiles.api.serializers import ProfileStatusSerializer
+from profiles.models import ProfileStatus
 
 
 class RegistrationTestCase(APITestCase):
